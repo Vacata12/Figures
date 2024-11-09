456971db901dc2252b75f28ecab429dfe15af8ad
Ivan Stoynev <79401987+Vacata12@users.noreply.github.com>
2024-11-10 00:20:05 +0200

    Added random figure generator

--- Headers/RandomGeneratorFigures.h
+++ Headers/RandomGeneratorFigures.h
@@ -4,9 +4,16 @@
 
 #ifndef RANDOMGENERATORFIGURES_H
 #define RANDOMGENERATORFIGURES_H
-
+#include <random>
+#include "../Headers/Circle.h"
+#include "../Headers/Rectangle.h"
+#include "../Headers/Triangle.h"
 class RandomGeneratorFigures {
-
+private:
+    static double min;
+    static double max;
+public:
+    static Figure* createRandomFigure();
 };
 
 
--- src/RandomGeneratorFigures.cpp
+++ src/RandomGeneratorFigures.cpp
@@ -2,3 +2,28 @@
 // Created by Ivan Stoynev on 9.11.24.
 //
 #include "../Headers/RandomGeneratorFigures.h"
+
+
+double RandomGeneratorFigures::min = 0;
+double RandomGeneratorFigures::max = 1000;
+
+Figure * RandomGeneratorFigures::createRandomFigure() {
+    int figure = std::rand() % 3;
+    std::uniform_real_distribution<double> unif(min,max);
+    std::default_random_engine re;
+    switch (figure) {
+        case 0:
+            return new Circle(unif(re));
+        case 1:
+            return new Rectangle (unif(re), unif(re));
+        case 2:
+            return new Triangle (unif(re), unif(re), unif(re));
+    }
+    return nullptr;
+}
+//saw that method here: https://www.geeksforgeeks.org/generate-random-double-numbers-in-cpp/
+// std::uniform_real_distribution<double> unif(min,max);
+// std::default_random_engine re;
+// double random_double = unif(re);
+// Time complexity: O(1).
+// Auxiliary Space: O(1).
